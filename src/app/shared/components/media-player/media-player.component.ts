b7c15729336edb01c0617c1dc2f9005b1e991f47
lalmiron <lauraandreaalmiron12@gmail.com>
2022-08-29 22:45:50 -0300

    scaffolding and basic modules creation

---
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'psf-media-player',
+  templateUrl: './media-player.component.html',
+  styleUrls: ['./media-player.component.css']
+})
+export class MediaPlayerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
