324917dada593094feeb5223f1b04539694831b5
Pedro Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-09-04 17:21:29 -0300

    import de modules en home

--- src/app/modules/home/home-routing.module.ts
+++ src/app/modules/home/home-routing.module.ts
@@ -2,10 +2,19 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'traks',
+    loadChildren: () => import('@modules/tracks/tracks.module').then(m => m.TracksModule)
+  },
+  {
+    path: 'history',
+    loadChildren: () => import('@modules/history/history.module').then(m => m.HistoryModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class HomeRoutingModule { }
