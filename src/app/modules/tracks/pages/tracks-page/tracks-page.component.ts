aaf411726e66ad8ec3aff5356820934081047796
lalmiron <lauraandreaalmiron12@gmail.com>
2022-09-04 23:27:34 -0300

    mejoras a section generic

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,25 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TrackModel } from '@core/models/tracks.model';
+import * as dataRaw from '../../../../data/track.model.json';
 @Component({
   selector: 'psf-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrls: ['./tracks-page.component.css']
 })
 export class TracksPageComponent implements OnInit {
-  mockTracksList = [
-    {
-      name: 'Madonna'
-    },
-    {
-      name: 'Madonna'
-    },
-    {
-      name: 'Madonna'
-    }
+  mockTracksList: Array<TrackModel> = [
   ]
   constructor() { }
 
   ngOnInit(): void {
+    const {data}:any = (dataRaw as any).default;
+    this.mockTracksList = data;
   }
 
 }
--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
 
 @Component({
   selector: 'psf-media-player',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./media-player.component.css']
 })
 export class MediaPlayerComponent implements OnInit {
-  mockCover: any = {
+  mockCover: TrackModel = {
     cover: 'https://los40es00.epimg.net/los40/imagenes/2017/11/28/album/1511885438_220718_1512034279_album_normal.jpg',
     album: 'Vogue',
-    name: 'Madonna'
+    name: 'Madonna',
+    url: 'http"//localhost/track.mp3',
+    _id: 1
   }
 
   constructor() { }
