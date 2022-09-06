de83f191f739cd50b6ca0d880aa1a49502ef969e
Diego Aguirre <aguirrediego734@gmail.com>
2022-09-05 21:29:59 -0300

    vista de login

--- src/app/modules/auth/pages/auth-page/auth-page.component.ts
+++ src/app/modules/auth/pages/auth-page/auth-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '@modules/auth/services/auth.service';
 
 @Component({
   selector: 'psf-auth-page',
@@ -8,7 +9,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 })
 export class AuthPageComponent implements OnInit {
   formLogin: FormGroup = new FormGroup({});
-  constructor() { }
+  constructor(private _authServices: AuthService) { }
 
   ngOnInit(): void {
     this.formLogin = new FormGroup(
@@ -26,7 +27,7 @@ export class AuthPageComponent implements OnInit {
     )
   }
   sendLogin(): void {
-    const body = this.formLogin.value
-    //TODO validar con be
+    const {email, password} = this.formLogin.value
+    this._authServices.sendCredentials(email, password)
   }
 }
