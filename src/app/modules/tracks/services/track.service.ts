7882db0ea0751147b77d7b9b55682fbe9b4f280f
lalmiron <lauraandreaalmiron12@gmail.com>
2022-09-07 21:34:59 -0300

    2do section generic

---
+++ src/app/modules/tracks/services/track.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { TrackModel } from '@core/models/tracks.model';
+import { Observable, of } from 'rxjs';
+import * as dataRaw from '../../../data/track.model.json'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TrackService {
+  dataTracksTrending$: Observable<TrackModel[]> = of([])
+  constructor() {
+    const {data}: any= (dataRaw as any).default
+    this.dataTracksTrending$ = of()
+  }
+}
