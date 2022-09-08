d9a837a6690a02a722094dde9ff7f35ef3f7bdce
lalmiron <lauraandreaalmiron12@gmail.com>
2022-09-07 23:57:46 -0300

    img brojen para card player big

--- src/app/shared/directives/img-broken.directive.ts
+++ src/app/shared/directives/img-broken.directive.ts
@@ -1,10 +1,15 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
-  selector: '[psfImgBroken]'
+  selector: 'img[psfImgBroken]'
 })
 export class ImgBrokenDirective {
-
-  constructor() { }
+  @HostListener('error') handleError(): void {
+    const elNative = this.elHost.nativeElement
+    elNative.src = '../../../assets/img_broken_logo.png'
+  }
+  constructor(private elHost: ElementRef) { 
+    console.log(this.elHost);
+  }
 
 }
