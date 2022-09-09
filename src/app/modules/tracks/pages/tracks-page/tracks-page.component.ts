3d191be42a1757cc0dbc64b5e06bafe8c1470353
lalmiron <lauraandreaalmiron12@gmail.com>
2022-09-08 23:14:08 -0300

    iniciando conexion de servicios

--- src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
+++ src/app/modules/tracks/pages/tracks-page/tracks-page.component.ts
@@ -1,19 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
+import { TrackService } from '@modules/tracks/services/track.service';
+import { Subscription } from 'rxjs';
 import * as dataRaw from '../../../../data/track.model.json';
 @Component({
   selector: 'psf-tracks-page',
   templateUrl: './tracks-page.component.html',
   styleUrls: ['./tracks-page.component.css']
 })
-export class TracksPageComponent implements OnInit {
-  mockTracksList: Array<TrackModel> = [
-  ]
-  constructor() { }
+export class TracksPageComponent implements OnInit, OnDestroy {
+  tracksTrending: Array<TrackModel> = []
+  tracksRandom: Array<TrackModel> = []
+  listObservers$: Array<Subscription> = []
+
+  constructor(private trackService: TrackService) { }
 
   ngOnInit(): void {
-    const {data}:any = (dataRaw as any).default;
-    this.mockTracksList = data;
+    const observer1$ = this.trackService.dataTracksTrending$.subscribe( response => {
+      this.tracksTrending = response
+      console.log("consiones tranding");
+    })
+
+    const observer2$ = this.trackService.dataTracksRandom$.subscribe( response => {
+      this.tracksRandom = [...this.tracksRandom, ... response]
+      console.log("consiones rambom");
+    })
+
+    this.listObservers$ = [observer1$, observer2$]
+  }
+
+  ngOnDestroy(): void {
+    this.listObservers$.forEach(u => u.unsubscribe)
   }
 
 }
--- src/app/modules/tracks/services/track.service.ts
+++ src/app/modules/tracks/services/track.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
-import { Observable, of } from 'rxjs';
+import { observable, Observable, of } from 'rxjs';
 import * as dataRaw from '../../../data/track.model.json'
 
 @Injectable({
@@ -8,8 +8,24 @@ import * as dataRaw from '../../../data/track.model.json'
 })
 export class TrackService {
   dataTracksTrending$: Observable<TrackModel[]> = of([])
+  dataTracksRandom$: Observable<any> = of([])
+
   constructor() {
     const {data}: any= (dataRaw as any).default
-    this.dataTracksTrending$ = of()
+    this.dataTracksTrending$ = of(data)
+    this.dataTracksRandom$ = new Observable((observer) => {
+      const trackExample: TrackModel = {
+        _id: 9,
+        name:"vogue",
+        album: "madonna",
+        url: "http://",
+        cover: "https://i.discogs.com/mD0y-Ti5vRtUn3PAz-uM_NGFWP5Rle8g2sA_IST7Vjc/rs:fit/g:sm/q:90/h:590/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTcxMTM3/LTEyMjM2NTM3NTQu/anBlZw.jpeg"
+      }
+
+      
+      setTimeout( () => {
+        observer.next([trackExample])
+      }, 3500)
+    })
   }
 }
