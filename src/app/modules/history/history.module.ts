2c5126c98d288dc614f8a63bb37905fb0ec2434d
Nicolas Bordon <nbordon@frba.utn.edu.ar>
2022-09-09 02:10:50 -0300

    page history y search component

--- src/app/modules/history/history.module.ts
+++ src/app/modules/history/history.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HistoryRoutingModule } from './history-routing.module';
-
+import { SearchComponent } from './components/search/search.component';
+import { HistoryPageComponent } from './pages/history-page/history-page.component';
+import { SharedModule } from '@shared/shared.module';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SearchComponent,
+    HistoryPageComponent
+  ],
   imports: [
     CommonModule,
-    HistoryRoutingModule
+    HistoryRoutingModule,
+    SharedModule
   ]
 })
 export class HistoryModule { }
